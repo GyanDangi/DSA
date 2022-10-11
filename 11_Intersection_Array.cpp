887c81bf738da87b992d416aae9b102b8ddd9586
Gyanprakash <gyanprakashdangi@gmail.com>
2022-10-11 21:41:10 +0530

    Intersection of 2 Array using Vector

--- 11_Intersection_Array.cpp
+++ 11_Intersection_Array.cpp
@@ -32,9 +32,34 @@ vector<int> printVector(vector<int> v){
 
 int main()
 {
-    vector<int> v={1,2,3,4,5};
-    // printVector(v);
-    vector<int> v1={1,2,3,4,5,6};
+    // vector<int> v={1,2,3,4,5};
+    // // printVector(v);
+    // vector<int> v1={1,2,3,4,5,6};
+    int n;
+    cout<<"enter the size of vector 1:"<<endl;
+    cin>>n;
+    int m;
+    cout<<"enter the size of vector 2 :"<<endl;
+    cin>>m;
+    vector<int>v;
+     
+    cout<<"enter vector1 elements:"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    vector<int>v1;
+    cout<<"enter vector2 elements:"<<endl;
+    for (int i = 0; i < m; i++)
+    {
+        int x;
+        cin>>x;
+        v1.push_back(x);
+    }
+    
 
     vector<int> ans= intesection(v, v1);
     printVector(ans);
