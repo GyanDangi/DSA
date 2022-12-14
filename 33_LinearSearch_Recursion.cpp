c4f59a3d9211ae11fd6cd239fc90d71e403b0e56
Gyanprakash <gyanprakashdangi@gmail.com>
2022-12-14 22:51:43 +0530

    LinearSearch Using Recursion

---
+++ 33_LinearSearch_Recursion.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+bool LinerSearch(int *arr, int size, int k)
+{
+
+    if (size == 0)
+    {
+        return false;
+    }
+    if (arr[0] == k)
+    {
+        return true;
+    }
+    else
+    {
+        bool remainingPart = LinerSearch(arr + 1, size - 1, k);
+        return remainingPart;
+    }
+}
+
+int main()
+{
+
+    int arr[5] = {1, 2, 4, 3, 2};
+    int size = 5;
+    int key = 8;
+    bool ans = LinerSearch(arr, size, key);
+
+    if (ans)
+    {
+        cout << "Present" << endl;
+    }
+    else
+    {
+        cout << "Absent" << endl;
+    }
+
+    return 0;
+}
