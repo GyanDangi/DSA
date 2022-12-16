fa10176a6c9591c9ff335662fb8cfb5024adf34f
Gyanprakash <gyanprakashdangi@gmail.com>
2022-12-16 12:18:49 +0530

    BubbleSort

--- 17_bubble_Sort.cpp
+++ 17_bubble_Sort.cpp
@@ -11,7 +11,7 @@ void printArray(int arr[], int size){
 void bubbleSort(int arr[], int n){
 
     for(int i=0; i<n-1; i++){
-        for (int j = 0; j < n-i; j++)
+        for (int j = 0; j < n-i-1; j++)
         {
             // Process Elements till n-1 index
             if(arr[j]>arr[j+1]){
