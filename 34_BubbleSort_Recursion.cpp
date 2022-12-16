0c7879d1c6681dba9f9ec3ff802a3a10a032aacc
Gyanprakash <gyanprakashdangi@gmail.com>
2022-12-16 13:19:44 +0530

    BubbleSort Using recursion

--- 34_BubbleSort_Recursion.cpp
+++ 34_BubbleSort_Recursion.cpp
@@ -11,6 +11,7 @@ void BubbleSort(int *arr, int n)
         return;
     }
 
+    // 1 case ko solve kr liya .. ith element ko uski last palace pr rakh diya.
     for (int i = 0; i < n - 1; i++)
     {
         if (arr[i] > arr[i + 1])
