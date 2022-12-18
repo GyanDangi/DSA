b8f51e35b34d3073c4e81f3979acc75c0b9bd557
Gyanprakash <gyanprakashdangi@gmail.com>
2022-12-18 20:10:30 +0530

    Seive Of Eratotesthenes

---
+++ 24_Seive_of_Erathosthenes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countPrime(int n){
+
+    int count=0;
+    vector<bool> prime(n+1,true);
+    prime[0]=prime[1]=false;
+
+    for (int i = 2; i < n; i++)
+    {
+        if(prime[i]){
+            count++;
+
+            for (int j = 2*i; j < n; j+i)
+            {
+                prime[j]=0;
+            }
+            
+        }
+    }
+    return count;
+    
+}
+
+int main(){
+
+
+    int ans=countPrime(40);
+    cout<<ans;
+    return 0;
+}
