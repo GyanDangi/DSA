6c18e99bc7e5111f9e15d6c60954b883137e6b06
Gyanprakash <gyanprakashdangi@gmail.com>
2023-01-03 21:06:35 +0530

    Getter And Setter In OOPs

--- 42_OopsINtro.cpp
+++ 42_OopsINtro.cpp
@@ -11,7 +11,7 @@ class Human{
     // Properties:
     // by default the data memeber are private:
     // within class we can access private data members:
-    char Name;
+    string Name;
     int age;
     // char gender;
     public:
@@ -19,22 +19,68 @@ class Human{
     int height;
     int wieght;
 
-    void Print(){
+   
+    // Getter and setter: to access private data memebers outside of class:
 
-        cout<<age<<endl;
+    // Setter:
+    void setName(string s){
+        Name=s;
+    }
+
+    string getName(){
+        return Name;
+    }
+
+   
+     void Print(){
+
+        // cout<<age<<endl;
+        cout<<"height:"<<this->height<<endl;
+        cout<<"weight:"<<this->wieght<<endl;
 
     }
 
 };
 int main() {
+
+    
     // Object Creation of class Human:
+
     Human Gyan;
 
 
     //cout<<Gyan.age<<endl; // THis will throw error/ Private Access kr rahe hai isliye:
 
-     Gyan.height=90;
-    cout<<Gyan.height<<endl;
-    cout<<sizeof(Gyan)<<endl;
+    //  Gyan.height=90;
+    // cout<<Gyan.height<<endl;
+    // cout<<sizeof(Gyan)<<endl;
+
+
+    // Getter and  Setter:
+    Gyan.setName("GyanPrakash");
+    cout<<Gyan.getName()<<endl;
+    
+
+    // static allocation:
+    Human Baba;
+    Baba.height=124;
+    Baba.setName("Panditji");
+   cout<< Baba.getName()<<endl;
+    cout<<Baba.height<<endl;
+
+    // Dynamic Allocation:
+    Human *Ramesh= new Human;
+    Ramesh->height=78;
+    Ramesh->setName("RameshHariom");
+    cout<<Ramesh->getName()<<endl;
+
+    // Accessing data memebers:
+    cout<<Ramesh->height<<endl;
+    // Using dereference operator:
+    cout<<(*Ramesh).height<<endl;
+
+    
+
+   
 return 0;
 }
