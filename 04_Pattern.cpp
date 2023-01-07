160261306239bde7ef299b3d19a7b1e8437ea96e
Gyanprakash <gyanprakashdangi@gmail.com>
2023-01-07 11:59:59 +0530

    Patterns

---
+++ 04_Pattern.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+
+
+/*
+A B C D E
+  A B C D
+    A B C
+      A B
+        A
+*/
+
+int n;
+
+cout<<"Enter the number of rows:"<<endl;
+cin>>n;
+for (int i = 1; i <=n; i++)
+{   
+   char ch='A';
+    for ( int space = 1; space< i; space++)
+    {
+        cout<<"  ";
+    }
+    
+    for (int j = 0; j <n-i+1; j++)
+    {
+        cout<<ch<<" ";
+        ch++;
+    }
+    
+    cout<<endl;
+}
+
+
+
+
+
+
+return 0;
+}
