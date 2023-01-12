cd9f49e378fa4dfc0cf9e20db1263136a8e034b0
Gyanprakash <gyanprakashdangi@gmail.com>
2023-01-12 21:24:27 +0530

    Kth Largest Element in Array

---
+++ 09_Kthlartest.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size){
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void bubbleSort(int arr[], int n){
+
+    for(int i=0; i<n-1; i++){
+        bool Swapped=false;
+        for (int j = 0; j < n-i-1; j++)
+        {
+            // Process Elements till n-1 index
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                Swapped=true;
+            }
+        }
+        if (Swapped==false)
+        {
+            break;
+        }
+        
+        
+    }
+}
+
+int KthElement(int *arr, int n, int k){
+
+    bubbleSort(arr, n);
+    return arr[n-k];
+}
+
+
+int main(){
+
+    int arr[5]={3,4,5,1,2};
+    printArray(arr,5);
+    bubbleSort(arr, 5);
+    printArray(arr,5);
+    cout<<KthElement(arr, 5,3);
+
+
+    return 0;
+}
